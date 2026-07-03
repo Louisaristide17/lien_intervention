9dc916501e3ae37a93c922a658637b23c238a060
Louisaristide17 <aristidetano@gmail.com>
2026-07-03 12:47:42 +0000

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-ingredients = ['flour', 'louis', 'eggs', 'milk']
-out_of_stock = ['sugar', 'louis', 'butter']
-for ingredient in ingredients:
-    if ingredient in out_of_stock:
-        print(f" {ingredient} n'est pas en stock")
-        break
-    else:
-        print("tous les ingredients sont en stock")
